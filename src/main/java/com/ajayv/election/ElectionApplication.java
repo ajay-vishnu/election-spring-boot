ef4c09d15961d391be5f610c539fe864a42161b2
Ajay Vishnu V <ajayvishnu950@gmail.com>
2024-01-15 23:02:31 +0530

    First commit

---
+++ src/main/java/com/ajayv/election/ElectionApplication.java
@@ -0,0 +1,14 @@
+package com.ajayv.election;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class ElectionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ElectionApplication.class, args);
+  }
+
+}
